4e248259895a0027d94dc52bac04f4853eac04b2
Pawel Jozwicki <pawel.jozwicki@orange.com>
2018-12-17 10:52:49 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c176ad8abb6f91bacb1e6b4')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5c13a859c09a7f498411abc6')
+    }, {
+        $set: {
+            name: 'Paweł'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    client.close();
+});
