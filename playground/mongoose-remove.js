615737c2050a2c8c4cc126128dbce2d51efd3abf
Pawel Jozwicki <pawel.jozwicki@orange.com>
2018-12-19 12:14:04 +0100

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+// Todo.findOneAndRemove()
+// Todo.findByIdAndRemove()
+
+Todo.findByIdAndRemove('5c1a2428ea6d57458827bdc4').then((todo) => {
+    console.log(todo);
+});
